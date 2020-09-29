4e40a73201cb44107ba0b1a08ab1e8f0765c955c
Gang MinKyung <alal0614@naver.com>
2020-09-29 23:44:09 +0900

    Adding Controller...

--- routes.js
+++ routes.js
@@ -13,6 +13,7 @@ const CHANGE_PASSWORD = "/change-password";
 
 //VIDEOS
 const VIDEOS = "/videos";
+const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
@@ -24,13 +25,26 @@ const routes = {
   logout: LOGOUT,
   search: SEARCH,
   users: USERS,
-  user_detail: USER_DETAIL,
-  edit_profile: EDIT_PROFILE,
-  change_password: CHANGE_PASSWORD,
+  userDetail: (id) => {
+    if (id) {
+      return `/users/${id}`;
+    } else {
+      return USER_DETAIL;
+    }
+  },
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
   videos: VIDEOS,
-  video_detail: VIDEO_DETAIL,
-  edit_video: EDIT_VIDEO,
-  delete_video: DELETE_VIDEO,
+  upload: UPLOAD,
+  videoDetail: (id) => {
+    if (id) {
+      return `/videos/${id}`;
+    } else {
+      return VIDEO_DETAIL;
+    }
+  },
+  editVideo: EDIT_VIDEO,
+  deleteVideo: DELETE_VIDEO,
 };
 
 export default routes;
